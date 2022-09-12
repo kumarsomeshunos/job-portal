0f455ae21111f9d1da95d2cdb4ff5afb6bcfdf13
Somesh Kumar <kumarsomesh.unos@gmail.com>
2022-09-12 13:14:50 +0530

    add second carousel in why us section

--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const WhyUs = () => {
   return (
-    <div className="whyus">
+    <div>
+      <div className="whyus">
       <h2>Why work @ Manipal</h2>
       <div
         id="carouselExampleCaptions"
         className="carousel slide"
         data-bs-ride="carousel"
       >
-        
         <div className="carousel-inner">
           <div className="upperlayer">
             <h1>Manipal.</h1>
@@ -60,6 +60,64 @@ const WhyUs = () => {
           </div>
         </div>
       </div>
+    </div>
+    <div className="whyus">
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="upperlayer">
+            <h1>Manipal.</h1>
+            <h3>
+              The Manipal Education Group, with its heritage of excellence in
+              higher education for over 60 years, launched Manipal University,
+              Jaipur in 2011.
+            </h3>
+          </div>
+          <div className="carousel-item active">
+            <img
+              src="images/whyus.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="images/whyus.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="images/whyus.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
