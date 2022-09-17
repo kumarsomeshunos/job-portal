bd8cb61045558fc6a0c312bfa6da4b41e567cf7b
Somesh Kumar <kumarsomesh.unos@gmail.com>
2022-09-17 11:56:47 +0530

    add half form

---
+++ src/components/Form/PartFour.js
@@ -0,0 +1,184 @@
+import React from "react";
+
+const PartFour = () => {
+  return (
+    <>
+      <div className="col-md-1">
+        <label htmlFor="validationCustom04" className="form-label">
+          GATE
+        </label>
+        <ul class="pagination pagination-lg">
+          <li class="page-item active" aria-current="page">
+            <span class="page-link" style={{ "font-size": "0.5rem" }}>
+              Yes
+            </span>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" style={{ "font-size": "0.5rem" }}>
+              No
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-md-3">
+        <label htmlFor="validationCustom03" className="form-label">
+          Year
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom03"
+          required=""
+        />
+        <div className="invalid-feedback">Please provide your email.</div>
+      </div>
+      <div className="col-md-1">
+        <label htmlFor="validationCustom04" className="form-label">
+          UGC Net
+        </label>
+        <ul class="pagination pagination-lg">
+          <li class="page-item active" aria-current="page">
+            <span class="page-link" style={{ "font-size": "0.5rem" }}>
+              Yes
+            </span>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" style={{ "font-size": "0.5rem" }}>
+              No
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-md-3">
+        <label htmlFor="validationCustom03" className="form-label">
+          Year
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom03"
+          required=""
+        />
+        <div className="invalid-feedback">Please provide your email.</div>
+      </div>
+      <div className="col-md-1">
+        <label htmlFor="validationCustom04" className="form-label">
+          UGCC JRF
+        </label>
+        <ul class="pagination pagination-lg">
+          <li class="page-item active" aria-current="page">
+            <span class="page-link" style={{ "font-size": "0.5rem" }}>
+              Yes
+            </span>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" style={{ "font-size": "0.5rem" }}>
+              No
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-md-3">
+        <label htmlFor="validationCustom03" className="form-label">
+          Year
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom03"
+          required=""
+        />
+        <div className="invalid-feedback">Please provide your email.</div>
+      </div>
+      <div className="col-md-1">
+        <label htmlFor="validationCustom04" className="form-label">
+          UGC CSIR
+        </label>
+        <ul class="pagination pagination-lg">
+          <li class="page-item active" aria-current="page">
+            <span class="page-link" style={{ "font-size": "0.5rem" }}>
+              Yes
+            </span>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" style={{ "font-size": "0.5rem" }}>
+              No
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-md-3">
+        <label htmlFor="validationCustom03" className="form-label">
+          Year
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom03"
+          required=""
+        />
+        <div className="invalid-feedback">Please provide your email.</div>
+      </div>
+      <div className="col-md-1">
+        <label htmlFor="validationCustom04" className="form-label">
+          ICMR
+        </label>
+        <ul class="pagination pagination-lg">
+          <li class="page-item active" aria-current="page">
+            <span class="page-link" style={{ "font-size": "0.5rem" }}>
+              Yes
+            </span>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" style={{ "font-size": "0.5rem" }}>
+              No
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-md-3">
+        <label htmlFor="validationCustom03" className="form-label">
+          Year
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom03"
+          required=""
+        />
+        <div className="invalid-feedback">Please provide your email.</div>
+      </div>
+      <div className="col-md-1">
+        <label htmlFor="validationCustom04" className="form-label">
+          ICAR
+        </label>
+        <ul class="pagination pagination-lg">
+          <li class="page-item active" aria-current="page">
+            <span class="page-link" style={{ "font-size": "0.5rem" }}>
+              Yes
+            </span>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" style={{ "font-size": "0.5rem" }}>
+              No
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-md-3">
+        <label htmlFor="validationCustom03" className="form-label">
+          Year
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom03"
+          required=""
+        />
+        <div className="invalid-feedback">Please provide your email.</div>
+      </div>
+    </>
+  );
+};
+
+export default PartFour;
