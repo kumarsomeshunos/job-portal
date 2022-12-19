b26d0d1e2db6d0a88ad4839823f607fb59e1b7ea
Atharv Gulati <atharvgulati@192.168.1.12>
2022-12-19 22:51:51 +0530

    New

--- src/components/Form/FormMerger.js
+++ src/components/Form/FormMerger.js
@@ -1,20 +1,21 @@
-import React from "react";
-import PartOne from "./PartOne";
-import PartTwo from "./PartTwo";
-import PartThree from "./PartThree";
-import PartFour from "./PartFour";
-import PartFive from "./PartFive";
-import PartSeven from "./PartSeven";
-import PartTen from "./PartTen";
-import PartSix from "./PartSix";
-import PartEight from "./PartEight";
+import React from 'react';
+import PartOne from './PartOne';
+import PartTwo from './PartTwo';
+import PartThree from './PartThree';
+import PartFour from './PartFour';
+import PartFive from './PartFive';
+import PartSeven from './PartSeven';
+import PartTen from './PartTen';
+import PartSix from './PartSix';
+import PartEight from './PartEight';
+import PartTwelve from './PartTwelve';
 
 const FormMerger = (props) => {
   return (
-    <div className="container" style={{ "margin-top": "3rem" }}>
-      <form className="row g-3 needs-validation" noValidate="">
+    <div className='container' style={{ 'margin-top': '3rem' }}>
+      <form className='row g-3 needs-validation' noValidate=''>
         <h1>1. Application Information</h1>
-        <PartOne data={props?.data}/>
+        <PartOne data={props?.data} />
         <h1>2. Personal Information</h1>
         <PartTwo data={props?.data} />
         <h1>3. Academic / Professional Qualification</h1>
@@ -36,20 +37,20 @@ const FormMerger = (props) => {
         <h1>9. Patent Details (in last three years) </h1>
         <PartTen data={props?.data} />
         <h1>12. Other Information </h1>
-        <PartTen />
-        <div className="col-12">
-          <div className="form-check">
+        <PartTwelve />
+        <div className='col-12'>
+          <div className='form-check'>
             <input
-              className="form-check-input"
-              type="checkbox"
-              defaultValue=""
-              id="invalidCheck"
-              required=""
+              className='form-check-input'
+              type='checkbox'
+              defaultValue=''
+              id='invalidCheck'
+              required=''
             />
-            <label className="form-check-label" htmlFor="invalidCheck">
+            <label className='form-check-label' htmlFor='invalidCheck'>
               Agree to terms and conditions
             </label>
-            <div className="invalid-feedback">
+            <div className='invalid-feedback'>
               You must agree before submitting.
             </div>
           </div>
